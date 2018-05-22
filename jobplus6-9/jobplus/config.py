eaac11b08f65761563a13f295519518082bb6169
monster-shang <jshang@live.cn>
2018-05-22 22:45:12 +0800

    fix #X add info to README

--- jobplus6-9/jobplus/config.py
+++ jobplus6-9/jobplus/config.py
@@ -1,21 +1,19 @@
-class BaseCofig(object):
-	'''配置基类'''
-	SECRET_KEY = 'very secret key'
-	
+class BaseConfig(object):
+    """配置基类"""
+    SECRET_KEY = 'very secret key'
 
-class DevelopmentConfig(BaseCofig):
-	DEBUG = True
-	SQLALCHEMY_TRACK_MODIFICATIONS=False
+class DevelopmentConfig(BaseConfig):
+    """开发环境配置"""
+    DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = 'mysql+mysqldb://root@localhost:3306/jobplus?charset=utf8'
 
+class ProductionConfig(BaseConfig):
+    """生产环境配置"""
+    pass
 	
-	
-class ProductionConfig(BaseCofig):
-	"""生产环境配置"""
-	pass
-	
-class TestingConfig(BaseCofig):
-	""""测试环境设置"""
-	pass
+class TestingConfig(BaseConfig):
+    """"测试环境设置"""
+    pass
 	
 configs = {
 	'development':DevelopmentConfig,
