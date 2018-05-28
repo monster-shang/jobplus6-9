a137b06f7693455e40a2765afe0c4b62772e423b
monster-shang <37285686+monster-shang@users.noreply.github.com>
2018-05-29 01:36:25 +0800

    Update config.py

--- jobplus6-9/jobplus/config.py
+++ jobplus6-9/jobplus/config.py
@@ -1,11 +1,13 @@
 class BaseConfig(object):
     """配置基类"""
     SECRET_KEY = 'very secret key'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 class DevelopmentConfig(BaseConfig):
     """开发环境配置"""
     DEBUG = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = 'mysql+mysqldb://root@localhost:3306/jobplus?charset=utf8'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root@localhost:3306/jobplus?charset=utf8'
 
 
 class ProductionConfig(BaseConfig):
